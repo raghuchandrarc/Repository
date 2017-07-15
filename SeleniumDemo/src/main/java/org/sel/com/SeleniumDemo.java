ffbf0635fca1e326826ee3f453cf811774397ff9
raghuchandrarc <raghuchandrarc@gmail.com>
2017-07-15 10:50:55 +0530

    first

---
+++ SeleniumDemo/src/main/java/org/sel/com/SeleniumDemo.java
@@ -0,0 +1,46 @@
+package org.sel.com;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumDemo {
+	@Test
+	public void testSeleniumDemo(){
+		 // declaration and instantiation of objects/variables
+    	WebDriver driver ;
+    	//System.setProperty("webdriver.firefox.marionette","F:\\eclipse-jee-neon-M7-win32-x86_64\\eclipse\\raghu\\SeleniumDemo\\src\\main\\resources\\drivers\\geckodriver.exe");
+    	File file = new File("F:\\DriverServer\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver",file.getAbsolutePath() );
+		
+    	driver = new FirefoxDriver();
+        String baseUrl = "http://newtours.demoaut.com";
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+       
+        // exit the program explicitly
+    }
+	}
+
+
